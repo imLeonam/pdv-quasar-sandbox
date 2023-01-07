5427028eaa63918292abd63ba043cf4ba7c8e8ec
imLeonam <leonammrib@gmail.com>
2023-01-07 01:18:52 -0300

    initial commit

---
+++ src/layouts/PdvLayout.vue
@@ -0,0 +1,121 @@
+<template>
+  <q-page class="row justify-start q-pa-md">
+    <div class="row">
+      <div class="col">
+        <GenericCard title="Adicionar Item">
+          <template v-slot:content>
+            <div class="row">
+              <div class="col q-mr-md">
+                <q-input outlined label="Quantidade"></q-input>
+              </div>
+              <div class="col">
+                <q-input outlined label="Código de Barras/Cód"></q-input>
+              </div>
+            </div>
+            <div class="row q-mt-md">
+              <div class="col-7">
+                <q-item-label>Produto</q-item-label>
+                <q-item-label>{{ product }}</q-item-label>
+              </div>
+              <div class="col-2">
+                <q-item-label>Valor Unitário</q-item-label>
+              </div>
+              <div class="col-1"><q-item-label> Qtd.</q-item-label></div>
+              <div class="col-2">
+                <q-item-label> Valor Total </q-item-label>
+              </div>
+            </div>
+          </template>
+          <template v-slot:actions>
+            <q-space />
+            <q-btn
+              icon="close"
+              rounded
+              label="Cancelar // Ctrl + Esc"
+              color="negative"
+            />
+            <q-btn
+              icon="check"
+              rounded
+              label="Finalizar // Ctrl + Enter"
+              color="positive"
+            />
+          </template>
+        </GenericCard>
+        <GenericCard title="Atalhos">
+          <template v-slot:content>
+            <div class="row">
+              <q-btn
+                class="q-ma-sm"
+                rounded
+                size="md"
+                icon="person"
+                label="Cliente // F5"
+                color="primary"
+              ></q-btn>
+              <q-btn
+                class="q-ma-sm"
+                rounded
+                size="md"
+                icon="error"
+                label="Remover Item // F6"
+                color="orange"
+                text-color="black"
+              ></q-btn>
+              <q-btn
+                class="q-ma-sm"
+                rounded
+                size="md"
+                icon="sell"
+                label="Vendas // F7"
+                color="primary"
+              ></q-btn>
+              <q-btn
+                class="q-ma-sm"
+                rounded
+                size="md"
+                icon="description"
+                label="Notas Fiscais // F8"
+                color="primary"
+              ></q-btn>
+              <q-btn
+                class="q-ma-sm"
+                rounded
+                size="md"
+                icon="close"
+                label="Sair // F9"
+                color="negative"
+              ></q-btn>
+            </div>
+          </template>
+        </GenericCard>
+      </div>
+      <div class="col">
+        <DataTable selectable title="Items" />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+import GenericCard from '@sharedComponents/dynamic/GenericCard.vue';
+import DataTable from '@sharedComponents/DataTableGrid.vue';
+
+export default defineComponent({
+  name: 'PdvLayout',
+  components: { GenericCard, DataTable },
+  methods: {
+    sayHello() {
+      console.log('hello');
+    },
+  },
+  setup() {
+    return {
+      cpf: ref('108.634.709.98'),
+      product: ref(''),
+    };
+  },
+});
+</script>
---
+++ src/shared/components/dynamic/GenericCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-card class="qcard--border q-mt-md" :style="`width: ${width}`">
+    <q-card-section class="qcard-title">
+      {{ title }}
+      <p v-if="subtitle">{{ subtitle }}</p>
+    </q-card-section>
+    <q-card-section>
+      <slot name="content" />
+    </q-card-section>
+    <q-card-actions class="qcard--actions">
+      <slot name="actions" />
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'GenericCard',
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+    subtitle: {
+      type: String,
+      required: false,
+    },
+    width: {
+      type: String,
+      default: '100%',
+    },
+  },
+});
+</script>
+
+<style>
+.qcard--border {
+  border-radius: 15px;
+}
+.qcard--actions {
+  background: rgb(197, 197, 197);
+}
+.qcard-title {
+  font-family: Helvetica;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 37px;
+  letter-spacing: 0.01em;
+  text-align: left;
+  color: rgb(25, 25, 25);
+  background: rgb(197, 197, 197);
+}
+</style>
